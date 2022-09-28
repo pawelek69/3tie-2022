ce2c25d7bb1b0932ecf921a9ef981b98d70f67ea
pawelek69 <97956110+pawelek69@users.noreply.github.com>
2022-09-28 11:34:31 +0200

    Add files via upload

--- skrypt.js
+++ skrypt.js
@@ -2,10 +2,14 @@ var iloscZn=8;
 var iloscWolnychZnakow=0;
 var iloscDuzeL=0;
 var iloscMaleL=0;
+var iloscZnaki=0;
+var iloscCyfry=0;
 function ustawWolne(){
-    iloscWolnychZnakow=+iloscZn-(+iloscDuzeL+ +iloscMaleL);
+    iloscWolnychZnakow=+iloscZn-(+iloscDuzeL+ +iloscMaleL+ +iloscZnaki+ +iloscCyfry);
     document.getElementById("iloscDuzeL").max=+iloscWolnychZnakow+ +iloscDuzeL;
     document.getElementById("iloscMaleL").max=+iloscWolnychZnakow+ +iloscMaleL;
+    document.getElementById("iloscZnaki").max=+iloscWolnychZnakow+ +iloscZnaki;
+    document.getElementById("iloscCyfry").max=+iloscWolnychZnakow+ +iloscCyfry;
 }
 function wybieranieDuzeL(){
     iloscDuzeL=document.getElementById("iloscDuzeL").value;
@@ -21,10 +25,22 @@ function iloscZnakow(){
     iloscZn=document.getElementById("iloscZnakow").value;
     ustawWolne()
 }
+function wybieranieZnaki(){
+    iloscZnaki=document.getElementById("iloscZnaki").value;
+    document.getElementById("wybieranieZnaki").value=iloscZnaki;
+    ustawWolne()
+}
+function wybieranieCyfry(){
+    iloscCyfry=document.getElementById("iloscCyfry").value;
+    document.getElementById("wybieranieCyfry").value=iloscCyfry;
+    ustawWolne()
+}
 function generujHaslo(){
     let haslo="";
     const alfabetMale="abcdefghijhklmnoprstuwxyz";
-    const alfabetDuze="ABCDEFGHIJKLMNOPRSTUWYXYZ"
+    const alfabetDuze="ABCDEFGHIJKLMNOPRSTUWYXYZ";
+    const alfabetZnaki="!@#$%^&*.<+";
+    const alfabetCyfry="123456789";
     //losowanie małych liter
     for(let i=0;i<iloscMaleL;i++){
     haslo+=alfabetMale.charAt(Math.floor(Math.random()*alfabetMale.length));
@@ -32,6 +48,14 @@ function generujHaslo(){
     //losowanie duzych liter
     for(let i=0;i<iloscDuzeL;i++){
         haslo+=alfabetDuze[Math.floor(Math.random()*alfabetDuze.length)];
+    }
+    //losowanie iloscZnaki
+       for(let i=0;i<iloscZnaki;i++){
+        haslo+=alfabetZnaki[Math.floor(Math.random()*alfabetZnaki.length)];
+    }
+    //alfabetcyfry
+    for(let i=0;i<iloscCyfry;i++){
+        haslo+=alfabetCyfry[Math.floor(Math.random()*alfabetCyfry.length)];
     }
     document.getElementById("haslo").value=zmianaKolejnosci(haslo);
 }
