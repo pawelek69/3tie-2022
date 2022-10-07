5c21379466f6bfe367b072044b73eca1301a7f13
pawelek69 <97956110+pawelek69@users.noreply.github.com>
2022-10-07 09:32:07 +0200

    Add files via upload

--- skrypt.js
+++ skrypt.js
@@ -1,14 +1,28 @@
 function euklidesOblicz(){
-   let liczbaA= parseInt (document.getElementById("liczbaA").value);
-   let liczbaB= parseInt (document.getElementById("liczbaB").value);
-   while(liczbaA!==liczbaB){
-       if(liczbaA>liczbaB){
-           liczbaA=liczbaA-liczbaB;
+    let liczbaA=parseInt(document.getElementById("liczbaA").value);
+    let liczbaB=parseInt(document.getElementById("liczbaB").value);
+    while(liczbaA!==liczbaB){
+        if(liczbaA>liczbaB){
+            liczbaA=liczbaA-liczbaB;
         }else{
-        liczbaB=liczbaB-liczbaA;
+           liczbaB=liczbaB-liczbaA; 
+        }
+
     }
+    document.getElementById("euklidesWynik").value=liczbaA;
+}
+function obliczNWW(){
+    let liczbaNWWA=parseInt(document.getElementById("liczbaNWWA").value);
+    let liczbaNWWB=parseInt(document.getElementById("liczbaNWWB").value);
+    let x=liczbaNWWA*liczbaNWWB;
+    
+    while(liczbaNWWB!==0){
+        c=liczbaNWWA%liczbaNWWB;
+        liczbaNWWA=liczbaNWWB;
+        liczbaNWWB=c;
+    }
+    let nww=x/liczbaNWWA;
+    document.getElementById("nwwWynik").value=nww;
 
-   }
-
-   document.getElementById("euklidesWynik").value=liczbaA;
+    
 }
